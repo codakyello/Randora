3b1c706aee0039d52ff8bca4d63bfffdd7fc30a8
Codakyello <olaoluwathompson8@gmail.com>
2024-12-07 23:16:42 +0100

    logo spacing

--- frontend/app/_components/Logo.tsx
+++ frontend/app/_components/Logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function Logo({ className }: { className?: string }) {
   return (
-    <h2 className={`font-semibold flex items-center gap-2 ${className}`}>
+    <h2 className={`font-semibold flex items-center gap-5 ${className}`}>
       <Image src="/logo.png" alt="logo" width={30} height={30} />
       <span>Randora</span>
     </h2>
