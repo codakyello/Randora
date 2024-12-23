11448d875ca64fd0afa65217208169498ff4e872
Codakyello <olaoluwathompson8@gmail.com>
2024-12-23 15:43:04 +0100

    use node js runtime

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    runtime: "nodejs", // Use Node.js runtime instead of Edge runtime
+  },
   images: {
     remotePatterns: [
       {
