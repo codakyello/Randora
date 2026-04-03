9a68698d0e1c4c9ca076627f057b885fca350d1e
codakyello <olaoluwathompson8@gmail.com>
2026-04-03 01:30:35 +0100

    update

--- frontend/app/_components/Login.tsx
+++ frontend/app/_components/Login.tsx
@@ -1,13 +1,6 @@
 "use client";
-import { useState } from "react";
 import LoginForm from "./LoginForm";
-import OtpForm from "./OTPForm";
 
 export default function Login() {
-  const [step, setStep] = useState<number>(1);
-  const [email, setEmail] = useState<string>("");
-
-  if (step === 1) return <LoginForm setEmail={setEmail} />;
-
-  if (step === 2) return <OtpForm step={step} onStep={setStep} email={email} />;
+  return <LoginForm />;
 }
